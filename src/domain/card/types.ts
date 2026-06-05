aa6d3883fcd5c57147661de7b8908ce0bcf7de78
nicolassteyer-afk <nicolassteyer1@gmail.com>
2026-06-05 14:54:04 +0200

    Add card domain types

---
+++ src/domain/card/types.ts
@@ -0,0 +1,36 @@
+export type BlockType = "text" | "image" | "section";
+
+export type TextBlockProps = {
+  text: string;
+  align: "left" | "center" | "right";
+};
+
+export type ImageBlockProps = {
+  src: string;
+  alt: string;
+  caption: string;
+};
+
+export type SectionBlockProps = {
+  title: string;
+  description: string;
+};
+
+export type BlockPropsByType = {
+  text: TextBlockProps;
+  image: ImageBlockProps;
+  section: SectionBlockProps;
+};
+
+export type CardBlock<TType extends BlockType = BlockType> = {
+  id: string;
+  type: TType;
+  props: BlockPropsByType[TType];
+};
+
+export type DigitalCard = {
+  id: string;
+  title: string;
+  blocks: CardBlock[];
+  updatedAt: string;
+};
